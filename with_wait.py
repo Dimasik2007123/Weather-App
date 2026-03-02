27e10ae159a67de8934884f432da2321ed91eed0
Dmitry <zdima4444@gmail.com>
2026-03-02 14:51:06 +0300

    Update git repo

---
+++ with_wait.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'with_wait.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Ожидайте, определяем ваш город")
+        Form.resize(500, 1)
+        Form.setMinimumSize(QSize(500, 1))
+        Form.setMaximumSize(QSize(500, 1))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Ожидайте, определяем ваш город", u"Ожидайте, определяем ваш город", None))
+    # retranslateUi
+
